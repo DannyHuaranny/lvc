ddc37118f8ea618791ac9ede40fbb4dc6d6ea55f
DannyHuaranny <58090793+DannyHuaranny@users.noreply.github.com>
2025-02-28 11:12:53 -0500

    Add files via upload

---
+++ lvc-main/web/20221008120800js_/https_/lvcsac.com.pe/Layouts/js/mail-script.js
@@ -0,0 +1,66 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+    // -------   Mail Send ajax
+
+     $(document).ready(function() {
+        var form = $('#myForm'); // contact form
+        var submit = $('.submit-btn'); // submit button
+        var alert = $('.alert-msg'); // alert div for show alert message
+
+        // form submit event
+        form.on('submit', function(e) {
+            e.preventDefault(); // prevent default form submit
+
+            $.ajax({
+                url: 'mail.php', // form action url
+                type: 'POST', // form submit method get/post
+                dataType: 'html', // request type html/json/xml
+                data: form.serialize(), // serialize form data
+                beforeSend: function() {
+                    alert.fadeOut();
+                    submit.html('Sending....'); // change submit button text
+                },
+                success: function(data) {
+                    alert.html(data).fadeIn(); // fade in response data
+                    form.trigger('reset'); // reset form
+                    submit.attr("style", "display: none !important");; // reset submit button text
+                },
+                error: function(e) {
+                    console.log(e)
+                }
+            });
+        });
+    });
+
+}
+/*
+     FILE ARCHIVED ON 12:08:00 Oct 08, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 22:32:19 Feb 05, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.719
+  exclusion.robots: 0.027
+  exclusion.robots.policy: 0.012
+  esindex: 0.015
+  cdx.remote: 7.989
+  LoadShardBlock: 375.573 (3)
+  PetaboxLoader3.datanode: 171.244 (5)
+  PetaboxLoader3.resolve: 322.984 (2)
+  load_resource: 148.509
+  loaddict: 48.09
+*/
